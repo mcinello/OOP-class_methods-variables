b5f4618dbe3fd6b10056f1fe7517530e35494f7d
Michelle Cinello <m.cinello@gmail.com>
2017-09-27 15:16:38 -0400

    created basic layout of class Zombie with the instance and class methods

---
+++ zombie.rb
@@ -0,0 +1,50 @@
+class Zombie
+
+  @@horde = [] #collection of zombies
+  @@plague_level 10 #determine rate at which new zombies are spawned (icnrease)
+  @@max_speed = 5 #max value for speed attribute of any zombie. This will NOT change
+  @@max_strength = 8 #max value for strength. won't change
+  @@default_speed = 1 #won't change
+  @@default_strength = 3 #won't change
+
+  #CLASS METHODS ----------------------------------------------------------
+
+  def self.all?
+
+  end
+
+  def self.new_day
+
+  end
+
+  def self.some_die_off
+
+  end
+
+  def self.spawn
+
+  end
+
+  def self.increase_plague_level
+
+  end
+
+  #INSTANCE METHODS ---------------------------------------------------
+
+  def initialize
+
+  end
+
+  def ecounter
+
+  end
+
+  def outrun_zombie?
+
+  end
+
+  def survive_attack?
+
+  end
+
+end
