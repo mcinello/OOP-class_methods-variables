0eb35407c7ac5e50dba8841753b2300e8a3d2a34
Michelle Cinello <m.cinello@gmail.com>
2017-09-27 19:08:18 -0400

    Figured out the bug. there has to be at least 3 zombies created because of zombie1, zombie2, zombie3. If there weren't at least 3 zombies, the program crashed.

--- zombie.rb
+++ zombie.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Zombie
 
   @@horde = [] #collection of zombies
@@ -14,29 +16,25 @@ class Zombie
   end
 
 #events of another day in the zombie apocalypse
-  def self.new_day
-    dead = Zombie.some_die_off
-    @@horde.delete_at(dead.to_i)
-
-    more_zombies = Zombie.spawn
-    @@horde << more_zombies
-
-    @@plague_level = Zombie.increase_plague_level
+  def self.new_day #delete vaariables and such. it's just generating new numbers. should look more simple
+    Zombie.some_die_off
+    Zombie.spawn
+    Zombie.increase_plague_level
   end
 
 #deletes zombies from array
   def self.some_die_off
-    @@horde.delete_at(rand(@@horde.length)) #should i do @@horde.length instead?
+    rand(11).times do
+      @@horde.delete_at(0)
+    end
   end
 
   #generates new zombies
   def self.spawn
-
-    rand(@@plague_level).times do
-    new_zombie = Zombie.new(rand(@@max_speed), rand(@@max_strength))
+    (rand(@@plague_level)+3).times do
+    new_zombie = Zombie.new((rand(@@max_speed)+1), (rand(@@max_strength)+1))
     @@horde << new_zombie
     end
-
   end
 
   def self.increase_plague_level
@@ -47,13 +45,17 @@ class Zombie
   #INSTANCE METHODS ---------------------------------------------------
 
   def initialize(speed, strength)
-    @speed = speed
-    @strength = strength
 
     if speed > @@max_speed
       @speed = @@default_speed
-    elsif strength >= @@max_strength
+    else
+      @speed = speed
+    end
+
+    if strength >= @@max_strength
       @strength = @@default_strength
+    else
+      @strength = strength
     end
 
   end
@@ -62,25 +64,25 @@ class Zombie
     #3 possible outcomes
 
     #1. escape unscathed
-    if self.outrun_zombie? == true
+    if outrun_zombie? == true && survive_attack? == true
       puts "You escaped!"
 
     #2. being killed by the zombie
-    elsif self.survive_attack? == false && self.outrun_zombie? == false
-    puts "You dead."
+    elsif survive_attack? == false
+      puts "You dead."
 
     #3 catching the plague and becoming a zombie yourself
-    elsif self.outrun_zombie? == false
-    new_zombie = Zombie.new(rand(@@max_speed), rand(@@max_strength))
-     @@horde << new_zombie
-     puts "You need braiiiiiiiiiiiiin."
+    elsif outrun_zombie? == false && survive_attack? == true
+      new_zombie = Zombie.new((rand(@@max_speed)+1), (rand(@@max_strength)+1)) #Zombie.spawn?
+       @@horde << new_zombie
+       puts "You need braiiiiiiiiiiiiin."
 
     end
-  #
   end
 
   def outrun_zombie?
-    human_speed = rand(@@max_speed)
+    human_speed = (rand(@@max_speed)+1)
+
     if human_speed > @speed
       return true
     else
@@ -89,7 +91,7 @@ class Zombie
   end
 
   def survive_attack?
-    am_i_strong = rand(@@max_strength)
+    am_i_strong = (rand(@@max_strength)+1)
     if am_i_strong > @strength
       return true
     else
@@ -97,28 +99,30 @@ class Zombie
     end
   end
 
+  def speed
+    return @speed
+  end
+
+  def strength
+    return @strength
+  end
+
 end
 
-
 puts Zombie.all.inspect # []
-
 Zombie.new_day
-
-
 puts Zombie.all.inspect # [#<Zombie:0x005626ecc5ebd0 @speed=4, @strength=0>, #<Zombie:0x005626ecc5eba8 @speed=0, @strength=4>, #<Zombie:0x005626ecc5eb80 @speed=4, @strength=4>]
-
 zombie1 = Zombie.all[0]
 zombie2 = Zombie.all[1]
 zombie3 = Zombie.all[2]
-
 puts zombie1.encounter # You are now a zombie
 puts zombie2.encounter # You died
 puts zombie3.encounter # You died
-# Zombie.new_day
-# puts Zombie.all.inspect # [#<Zombie:0x005626ecc5e1f8 @speed=0, @strength=0>, #<Zombie:0x005626ecc5e180 @speed=3, @strength=3>, #<Zombie:0x005626ecc5e158 @speed=1, @strength=2>, #<Zombie:0x005626ecc5e090 @speed=0, @strength=4>]
-# zombie1 = Zombie.all[0]
-# zombie2 = Zombie.all[1]
-# zombie3 = Zombie.all[2]
-# puts zombie1.encounter # You got away
-# puts zombie2.encounter # You are now a zombie
-# puts zombie3.encounter # You died
+Zombie.new_day
+puts Zombie.all.inspect # [#<Zombie:0x005626ecc5e1f8 @speed=0, @strength=0>, #<Zombie:0x005626ecc5e180 @speed=3, @strength=3>, #<Zombie:0x005626ecc5e158 @speed=1, @strength=2>, #<Zombie:0x005626ecc5e090 @speed=0, @strength=4>]
+zombie1 = Zombie.all[0]
+zombie2 = Zombie.all[1]
+zombie3 = Zombie.all[2]
+puts zombie1.encounter # You got away
+puts zombie2.encounter # You are now a zombie
+puts zombie3.encounter # You died
