7b8679a08edcfd2f175ca8f245efbb9c0136d5ca
Michelle Cinello <m.cinello@gmail.com>
2017-09-26 22:48:17 -0400

    switched code around; also edited the lent_out? method. It was breaking the code and it wasn't until I did 'puts sister_outsider.lent_out?' I realized it. It still returned true even when the book was not lent out. I switched what was true/false

--- booklending.rb
+++ booklending.rb
@@ -1,52 +1,9 @@
+require 'pry'
+
 class Book
 
 @@on_shelf =[] #collection of books available to lend out
 @@on_loan =[] #books currently being borrowed
-
-  def initialize(title, author, isbn)
-    @title = title
-    @author = author
-    @isbn = isbn
-  end
-
-  def due_date
-    @due_date = Book.current_due_date
-    return @due_date
-  end
-
-  def borrow
-    if self.lent_out? == true
-      return false
-    else
-      Book.current_due_date
-      @@on_shelf.delete(self)
-      @@on_loan << self
-      return true
-    end
-  end
-
-
-  def return_to_library
-    if self.lent_out? == true
-      @@on_loan.delete(self)
-      @@on_shelf << self
-      @due_date = nil
-      return true
-    else
-      return false
-    end
-  end
-
-# #true if book borrowed, false otherwise
-  def lent_out?
-    @@on_shelf.each do |book|
-      if self == book
-        return false
-      else
-        return true
-      end
-    end
-  end
 
 #CLASS METHODS ---------------------------------------------------------------
 
@@ -90,7 +47,56 @@ class Book
     return @@on_loan
   end
 
+  # INSTANCE METHODS ---------------------------------------------------------
 
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+  end
+
+  def due_date
+    @due_date
+    return @due_date
+  end
+
+  def due_date=(reset)
+    @due_date = reset
+    return @due_date
+  end
+
+  def borrow
+    if self.lent_out? == true
+      return false
+    else
+      @due_date = Book.current_due_date
+      @@on_shelf.delete(self)
+      @@on_loan << self
+      return true
+    end
+  end
+
+
+  def return_to_library
+    if self.lent_out? == true
+      @@on_loan.delete(self)
+      @@on_shelf << self
+      @due_date = nil
+      return true
+    else
+      return false
+    end
+  end
+
+# #true if book borrowed, false otherwise
+  def lent_out?
+    @@on_shelf.each do |book|
+      if self == book
+        return false
+      end
+    end
+    return true
+  end
 
 end
 
@@ -98,6 +104,7 @@ end
 sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
 aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
 if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
+
 puts Book.browse.inspect # #<Book:0x00555e82acdab0 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221"> (this value may be different for you)
 puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
 puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
